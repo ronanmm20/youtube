be34fd2233c40c016f0248feb20f216930c0dedb
Tehnikumi Opilane <opilane@tptlive.ee>
2021-12-16 11:41:52 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,213 @@
+<template>
+  <v-app dark>
+    
+    <v-app-bar
+      :clipped-left="true"
+      :bottom="bottom"
+      fixed
+      app
+    >
+    
+  
+      <v-icon class="pe-5" color="red">mdi-play-box</v-icon>
+      <v-toolbar-title v-text="title" ></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-text-field class="ms-5 mt-8 px-3 py-3"
+            label="Search..."
+            outlined
+      ></v-text-field>
+         <v-btn><v-icon class="px-1" color="red">mdi-magnify</v-icon></v-btn>
+         <v-btn><v-icon class="px-2" color="red">mdi-microphone</v-icon></v-btn>
+      <v-spacer></v-spacer>
+      <v-spacer />
+
+      <v-btn
+              icon
+            >
+              <v-icon color="red">mdi-apps</v-icon>
+      </v-btn>
+      <v-btn
+              icon
+            >
+              <v-icon color="red">mdi-dots-vertical</v-icon>
+      </v-btn>
+
+
+
+      <v-btn
+        outlined
+        color="red"
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-account</v-icon>
+        SIGN IN
+      </v-btn>
+    </v-app-bar>
+
+<v-navigation-drawer
+      v-model="drawer"
+      :clipped="true"
+      fixed
+      app
+      permanent
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+        
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+          
+          
+      
+        <v-spacer></v-spacer>
+    
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-compass',
+          title: 'Explore',
+          to: '/inspire'
+        },
+        {
+          icon: 'mdi-youtube-subscription',
+          title: 'Subscriptions',
+          to: '/subscribed'
+        },
+        {
+          icon: '',
+          title: '',
+          to: '/history'
+        },
+        {
+          icon: 'mdi-play-box-multiple',
+          title: 'Library',
+          to: '/history'
+        },
+        {
+          icon: 'mdi-history',
+          title: 'History',
+          to: '/history'
+        },
+        {
+          icon: 'mdi-music',
+          title: 'Music',
+          to: '/music'
+        },
+         {
+          icon: 'mdi-trophy',
+          title: 'Sports',
+          to: '/sport'
+        },
+        {
+          icon: 'mdi-youtube-gaming',
+          title: 'Gaming',
+          to: '/gaming'
+        },
+        {
+          icon: 'mdi-newspaper',
+          title: 'News',
+          to: '/news'
+        },
+        {
+          icon: 'mdi-access-point',
+          title: 'Live',
+          to: '/live'
+        },
+         {
+          icon: 'mdi-panorama-sphere-outline',
+          title: '360° Video',
+          to: '/360video'
+        },
+        {
+          icon: '',
+          title: '',
+          to: '/360video'
+        },
+        {
+          icon: 'mdi-plus-circle',
+          title: 'Browse channels',
+          to: '/browsechannels'
+        },
+        {
+          icon: 'mdi-youtube',
+          title: 'Youtube Premium',
+          to: '/youtubepremium'
+        },
+        {
+          icon: 'mdi-cog-outline',
+          title: 'Settings',
+          to: '/settings'
+        },
+        {
+          icon: 'mdi-flag-variant-outline',
+          title: 'Report history',
+          to: '/report'
+        },
+        {
+          icon: 'mdi-help-circle-outline',
+          title: 'Help',
+          to: '/help'
+        },
+        {
+          icon: 'mdi-message-alert-outline',
+          title: 'Send feedback',
+          to: '/Send feedback'
+        },
+      
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Youtube'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,137 @@
+<template>
+<div>
+    <v-sheet
+    class="mx-auto mb-2"
+  >
+    <v-slide-group
+      show-arrows
+    >
+      <v-slide-item
+        v-for="n in 25"
+        :key="n"
+        v-slot="{ active, toggle }"
+        class="mt-"
+      >
+        <v-btn
+          class="mx-2"
+          :input-value="active"
+          active-class="white--text"
+          depressed
+          rounded
+          @click="toggle"
+        >
+          {{ keywords[n] }}
+        </v-btn>
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>
+
+    <v-row no-gutters>
+      <v-card
+        v-for="n in 3"
+        :key="n"
+        class="pl-1 mx-5 my-2"
+        
+      >
+      <v-responsive
+          v-if="n === 2"
+          :key="`width-${n}`"
+          width="100%" 
+        ></v-responsive>
+    <v-img class="py-1"
+      src="https://i1.sndcdn.com/artworks-bXANGgDMELEJkNOM-ZqRY1g-t500x500.jpg"
+      height="230px"
+      width="250px"
+    ></v-img>
+
+    <v-card-title class="py-4">
+      exeter <v-icon>mdi-dots-vertical</v-icon>
+    </v-card-title>
+
+    <v-card-subtitle class="pa-0">
+      drain gang <v-icon>mdi-check-decagram</v-icon>
+    </v-card-subtitle>
+
+    <v-card-subtitle class="pa-0">
+      583k views 5 days ago
+    </v-card-subtitle>
+    </v-card>
+    </v-row>
+
+
+
+
+
+    <v-row no-gutters>
+      <v-card
+        v-for="n in 3"
+        :key="n"
+        class="pl-1 mx-5 my-2"
+      >
+      <v-responsive
+          v-if="n === 2"
+          :key="`width-${n}`"
+          width="100%" 
+        ></v-responsive>
+    <v-img class="py-2"
+      src="https://preview.redd.it/oax5lic6ny161.png?auto=webp&s=b95cb6da34a2aba03944c9e62b931713e0dd6677"
+      height="230px"
+      width="250px"
+    ></v-img>
+
+    <v-card-title class="py-4">
+      333 <v-icon>mdi-dots-vertical</v-icon>
+    </v-card-title>
+
+    <v-card-subtitle class="pa-0">
+      drain gang <v-icon>mdi-check-decagram</v-icon>
+    </v-card-subtitle>
+
+    <v-card-subtitle class="pa-0">
+      362k views 11 hour ago
+    </v-card-subtitle>
+    </v-card>
+    </v-row>
+
+    <v-row no-gutters>
+      <v-card
+        v-for="n in 3"
+        :key="n"
+        class="pl-1 mx-5 my-2"
+      >
+      <v-responsive
+          v-if="n === 2"
+          :key="`width-${n}`"
+          width="100%" 
+        ></v-responsive>
+    <v-img class="py-2"
+      src="https://img.youtube.com/vi/sNuMM_9UqZU/0.jpg"
+      height="230px"
+      width="250px"
+    ></v-img>
+
+    <v-card-title class="py-4">
+      gluee <v-icon>mdi-dots-vertical</v-icon>
+    </v-card-title>
+
+    <v-card-subtitle class="pa-0">
+      drain gang <v-icon>mdi-check-decagram</v-icon>
+    </v-card-subtitle>
+
+    <v-card-subtitle class="pa-0">
+      2M views 28 days ago
+    </v-card-subtitle>
+    </v-card>
+    </v-row>
+
+</div>
+</template>
+<script>
+
+export default {
+  data: () => ({
+    keywords: ['Music', 'Mixes', 'Laughter', 'COVID', 'Programming', 'League of Legends', 'Cryptocurrency', 'Rap', 'Paintball', 'Marvel', 'Tesla', 'Trailers', 'Reaction videos', 'Experiments', 'Outer space', 'Aliens'],
+  }),
+}
+</script>
+
